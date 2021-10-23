20cf1da5e2b6b7fd2120560c06c4d3c8f47a0a38
Anton Maminov <anton.linux@gmail.com>
2021-10-23 13:18:40 +0300

    format code

--- samples/readme.cr
+++ samples/readme.cr
@@ -5,7 +5,6 @@ if country = ISOCodes.countries.find_by_alpha_2("UA")
   puts country.inspect
   # => <ISOCodes::Country:0x7f4bd307e5a0 @alpha_2="UA", @alpha_3="UKR", @name="Ukraine", @numeric="804", @official_name=nil, @flag="🇺🇦", @common_name=nil>
 
-
   puts country.translation("pt")
   # => Ucrânia
 end
